f565784cb4474df8e0a222946ef9f33b9efe9da2
José Francisco Ferreira Alves de Sousa <josesousa9000@gmail.com>
2016-03-09 11:46:04 +0000

    simplificar a gestão de odds dentro de uma aposta
    Em vez de guardar todas as odds na aposta, guardamos só a odd correspondente ao resultado escolhido

--- api/aposta.rb
+++ api/aposta.rb
@@ -1,28 +1,27 @@
 class Aposta
-  attr_reader :evento, :homeodd, :drawodd, :awayodd, :result, :valor, :apostador, :premio # home/away
+  attr_reader :evento, :odd, :result, :valor, :apostador, :premio # home/away
 
   def initialize(evento, result, valor, apostador)
     @evento = evento
     @result = result
     @valor = valor
     @apostador = apostador
-    @homeodd = evento.homeodd
-    @drawodd = evento.drawodd
-    @awayodd = evento.awayodd
+    case result
+    when 'HOME'
+      @odd = evento.homeodd
+    when 'DRAW'
+      @odd = evento.drawodd
+    when 'AWAY'
+      @odd = evento.awayodd
+    end
     @premio = 0
   end
 
-  def update(result)
+  def update(_result)
     result = @evento.result
-    premio = 0
-    if @evento.result == @result
-      premio = @awayodd * @valor if @evento.result == 'AWAY'
-      premio = @homeodd * @valor if @evento.result == 'HOME'
-      premio = @drawodd * @valor if @evento.result == 'DRAW'
-    end
-    @premio = premio
-    apostador.balance += premio
-    puts "OBSERVER!!!! Apostador:#{apostador.email} ganhou #{premio} créditos"
+    @premio = @odd * @valor if @evento.result == @result
+    apostador.balance += @premio
+    puts "OBSERVER!!!! Apostador:#{apostador.email} ganhou #{@premio} créditos"
   end
 
   def to_s
